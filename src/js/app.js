3d6772faa0fecd4eda686c1620f21453714dfd1d
MyCode07 <erem19992015@mail.ru>
2024-05-01 18:40:15 +0300

    add another changes to wep page

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
 import "./parts/chart.js";
 import "./parts/tabs.js";
 import "./parts/menu.js";
-// import "./parts/scroll.js";
+import "./parts/scroll.js";
--- src/js/parts/scroll.js
+++ src/js/parts/scroll.js
@@ -1,28 +1,35 @@
 const scrollElems = document.querySelectorAll('.scroll-wrapper');
 if (scrollElems) {
     scrollElems.forEach(elem => {
+        const scrollElem = elem.querySelector('.scroll-wrapper__table');
+        const table = document.querySelector('.table');
+        const scrollbar = elem.querySelector('.scroll-wrapper__scrollbar');
         const scrollbarThumb = elem.querySelector('.scroll-wrapper__scrollbar span');
 
-        const mapWidth = elem.getBoundingClientRect().width;
         const containerWidth = elem.closest('section').querySelector('._container').getBoundingClientRect().width - 24;
+        const scrollElemWidth = table.getBoundingClientRect().width;
+        const scrollbarWidth = scrollbar.getBoundingClientRect().width;
+        const scrollbarThumbWidth = scrollbarThumb.getBoundingClientRect().width;
+        const scrollbarScrollPecent = 100 - scrollbarThumbWidth / scrollbarWidth * 100;
 
-        let scrollLeft = elem.scrollLeft
-        let scrollPercent = scrollLeft / (mapWidth - containerWidth)
+        let scrollLeft = scrollElem.scrollLeft
+        let scrollPercent = scrollLeft / (scrollElemWidth - containerWidth)
 
-        let mapVisibleWidth = containerWidth / mapWidth * 100
-        let mapHiddenWidth = 100 - mapVisibleWidth
-        let scrollBarScroll = mapHiddenWidth * scrollPercent
+        let scrollElemVisibleWidth = containerWidth / scrollElemWidth * 100
+        let scrollElemHiddenWidth = 100 - scrollElemVisibleWidth
+        let scrollBarScroll = scrollElemHiddenWidth * scrollPercent
 
 
-        scrollbarThumb.style.maxWidth = mapVisibleWidth + '%';
+        scrollbarThumb.style.maxWidth = scrollElemVisibleWidth + '%';
         scrollbarThumb.style.left = 0
 
         if (window.innerWidth <= 1024) {
-            elem.addEventListener('scroll', () => {
-                scrollLeft = elem.scrollLeft
-                scrollPercent = scrollLeft / (mapWidth - containerWidth)
-                scrollBarScroll = mapHiddenWidth * scrollPercent
-                scrollbarThumb.style.left = scrollBarScroll + '%';
+            scrollElem.addEventListener('scroll', () => {
+                scrollLeft = scrollElem.scrollLeft
+                scrollPercent = scrollLeft / (scrollElemWidth - containerWidth)
+                scrollBarScroll = scrollElemHiddenWidth * scrollPercent
+                // scrollbarThumb.style.left = scrollBarScroll + '%';
+                scrollbarThumb.style.left = scrollPercent * scrollbarScrollPecent + '%';
             })
         }
     });
